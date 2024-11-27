ee5c89e472f9b50a14e044686fd4432b97a0b218
Diego Pereira Betti <diegopbetti.gmail.com>
2024-11-27 20:15:32 -0300

    Cria a logica de virar os cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,8 @@
-import './Cards.css'
+import React, { useState } from "react";
+import "./Cards.css";
 
 function Cards() {
-  const emojis = [ // usando emojis como exemplo para teste
+  const emojis = [
     "😸",
     "😸",
     "🦝",
@@ -20,18 +21,62 @@ function Cards() {
     "🐮",
   ];
 
-  let cardsVirados = [];
-  let cardsEmbaralhados = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+  const [cardsEmbaralhados, setCardsEmbaralhados] = useState(
+    emojis
+      .sort(() => Math.random() - 0.5)
+      .map((emoji, indice) => ({
+        id: indice,
+        emoji: emoji,
+        estaVirado: false,
+        estaCerto: false,
+      }))
+  );
+
+  const [cardsVirados, setCardsVirados] = useState([]);
+
+  function checaIgualdade() {
+    const novosCards = [...cardsEmbaralhados];
+
+    if (novosCards[0].emoji === novosCards[1].emoji) {
+      novosCards[0].estaCerto = true;
+      novosCards[1].estaCerto = true;
+    } else {
+      novosCards[0].estaVirado = false;
+      novosCards[1].estaVirado = false;
+    }
+
+    setCardsEmbaralhados(novosCards);
+    setCardsVirados([]);
+  }
+
+  function handleClick(indice) {
+    if (cardsVirados.length < 2 && !cardsEmbaralhados[indice].estaVirado) {
+      const novosCards = cardsEmbaralhados.map((card, i) =>
+        i === indice ? { ...card, estaVirado: true } : card
+      );
+
+      setCardsEmbaralhados(novosCards);
+      setCardsVirados([...cardsVirados, indice]);
+
+      if (cardsVirados.length === 1) {
+        setTimeout(checaIgualdade, 500);
+      }
+    }
+  }
 
   return (
     <div className="cards-container">
-      {cardsEmbaralhados.map((item) => {
-        return(
-            <div className="card">
-                {item}
-            </div>
-        )
-      })}
+      {cardsEmbaralhados.map((card, indice) => (
+        <div
+          key={card.id}
+          className={`card ${card.estaVirado ? "card-virado" : ""} ${
+            card.estaCerto ? "acertou" : ""
+          }`}
+          onClick={() => handleClick(indice)}
+        >
+          {card.emoji}
+        </div>
+      ))}
     </div>
   );
 }

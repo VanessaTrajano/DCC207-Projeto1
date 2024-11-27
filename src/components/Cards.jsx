b0672a1819ab94b13892b6dd67184d969424fda1
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-11-27 09:12:05 -0300

    Cria map para mostrar os cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,36 @@
 //import './Cards.css'
 
 function Cards() {
+  const emojis = [ // usando emojis como exemplo para teste
+    "😸",
+    "😸",
+    "🦝",
+    "🦝",
+    "🦊",
+    "🦊",
+    "🐶",
+    "🐶",
+    "🐵",
+    "🐵",
+    "🦁",
+    "🦁",
+    "🐯",
+    "🐯",
+    "🐮",
+    "🐮",
+  ];
+
+  let cardsVirados = emojis; // por enquanto deixaremos assim para estilização
 
   return (
     <div>
-      <h3>funciona</h3>
+      {cardsVirados.map((item) => {
+        return(
+            <div>
+                {item}
+            </div>
+        )
+      })}
     </div>
   );
 }
